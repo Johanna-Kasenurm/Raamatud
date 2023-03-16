e0ca590f3b3680fb6048daec8c504113e3381f70
Johanna Kasenurm <johanna.kasenurm@gmail.com>
2023-03-16 13:11:45 +0200

    valiSuvaline

--- src/Nimekiri.java
+++ src/Nimekiri.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Nimekiri {
     protected List<Raamat> raamatud;
@@ -20,5 +21,11 @@ public class Nimekiri {
 
     protected void eemaldaRaamat(Raamat raamat) {
         raamatud.remove(raamat);
+    }
+
+    protected Raamat valiSuvaline() {
+        Random rand = new Random();
+        int indeks = rand.nextInt(0, raamatud.size());
+        return raamatud.get(indeks);
     }
 }
